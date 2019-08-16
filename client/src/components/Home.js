3ef821ff85b9ab2aace1f16f260c4081b54d13ca
ncen5293 <nickycen@hotmail.com>
2019-08-16 14:28:24 -0400

    created Home file

---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles/Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="projects-button">
+          My Projects
+        </div>
+      </div>
+    )
+
+  }
+}
+
+export default Home;
