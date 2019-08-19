23dcbb570d7d3a334074d174ad1915176eaf1767
ncen5293 <nickycen@hotmail.com>
2019-08-19 15:10:56 -0400

    made buttons responsive

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Image, Transition } from 'semantic-ui-react'
+import { Image, Transition } from 'semantic-ui-react';
+import HomeButtons from 'HomeButtons';
 import '../styles/Home.css';
 
 class Home extends Component {
@@ -13,12 +14,14 @@ class Home extends Component {
   }
 
   componentDidMount = () => {
-    this.setState(prevState => ({ visibleProjectButton: !prevState.visibleProjectButton }));
-    window.setTimeout(() => {this.setState(prevState => ({ visibleHobbiesButton: !prevState.visibleHobbiesButton }));}, 100);
-    window.setTimeout(() => {this.setState(prevState => ({ visibleAbilitiesButton: !prevState.visibleAbilitiesButton }));}, 200);
+    this.buttonVisibility();
   }
 
   onButtonClick = (type) => {
+    this.buttonVisibility();
+  }
+
+  buttonVisibility = () => {
     this.setState(prevState => ({ visibleProjectButton: !prevState.visibleProjectButton }));
     window.setTimeout(() => {this.setState(prevState => ({ visibleHobbiesButton: !prevState.visibleHobbiesButton }));}, 100);
     window.setTimeout(() => {this.setState(prevState => ({ visibleAbilitiesButton: !prevState.visibleAbilitiesButton }));}, 200);
@@ -33,48 +36,12 @@ class Home extends Component {
         <h1>
           Nicky Cen
         </h1>
-        <Transition visible={visibleProjectButton} animation='vertical flip' duration={500}>
-          <div>
-            <div className='projects-button top-left' onClick={() => this.onButtonClick('projects')} >
-              <h2 className='inner-projects-button'>
-                My Projects
-              </h2>
-            </div>
-            <div className='right-projects-button top-right' onClick={() => this.onButtonClick('unavailable')} >
-              <h2 className='right-inner-projects-button'>
-                Coming Soon
-              </h2>
-            </div>
-          </div>
-        </Transition>
-        <Transition visible={visibleHobbiesButton} animation='vertical flip' duration={700}>
-          <div>
-            <div className='projects-button middle-left' onClick={() => this.onButtonClick('hobbies')} >
-              <h2 className='inner-projects-button'>
-                My Hobbies
-              </h2>
-            </div>
-            <div className='right-projects-button middle-right' onClick={() => this.onButtonClick('unavailable')} >
-              <h2 className='right-inner-projects-button'>
-                Coming Soon
-              </h2>
-            </div>
-          </div>
-        </Transition>
-        <Transition visible={visibleAbilitiesButton} animation='vertical flip' duration={900}>
-          <div>
-            <div className='projects-button bottom-left' onClick={() => this.onButtonClick('abilities')} >
-              <h2 className='inner-projects-button'>
-                My Abilities
-              </h2>
-            </div>
-            <div className='right-projects-button bottom-right' onClick={() => this.onButtonClick('unavailable')} >
-              <h2 className='right-inner-projects-button'>
-                Coming Soon
-              </h2>
-            </div>
-          </div>
-        </Transition>
+        <HomeButtons
+          visibleProjectButton={visibleProjectButton}
+          visibleAbilitiesButton={visibleAbilitiesButton}
+          visibleHobbiesButton={visibleHobbiesButton}
+          onButtonClick={this.onButtonClick}
+        />
       </div>
     )
 
---
+++ client/src/components/HomeButtons.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Image, Transition } from 'semantic-ui-react';
+import '../styles/Home.css';
+
+class HomeButtons extends Component {
+  render() {
+    const visibleProjectButton = this.props.visibleProjectButton;
+    const visibleAbilitiesButton = this.props.visibleAbilitiesButton;
+    const visibleHobbiesButton = this.props.visibleHobbiesButton;
+    return (
+      <div>
+        <Transition visible={visibleProjectButton} animation='vertical flip' duration={500}>
+          <div>
+            <div className='projects-button top-left' onClick={() => this.props.onButtonClick('projects')} >
+              <h2 className='inner-projects-button'>
+                My Projects
+              </h2>
+            </div>
+            <div className='right-projects-button top-right' onClick={() => this.props.onButtonClick('unavailable')} >
+              <h2 className='right-inner-projects-button'>
+                Coming Soon
+              </h2>
+            </div>
+          </div>
+        </Transition>
+        <Transition visible={visibleHobbiesButton} animation='vertical flip' duration={700}>
+          <div>
+            <div className='projects-button middle-left' onClick={() => this.props.onButtonClick('hobbies')} >
+              <h2 className='inner-projects-button'>
+                My Hobbies
+              </h2>
+            </div>
+            <div className='right-projects-button middle-right' onClick={() => this.props.onButtonClick('unavailable')} >
+              <h2 className='right-inner-projects-button'>
+                Coming Soon
+              </h2>
+            </div>
+          </div>
+        </Transition>
+        <Transition visible={visibleAbilitiesButton} animation='vertical flip' duration={900}>
+          <div>
+            <div className='projects-button bottom-left' onClick={() => this.props.onButtonClick('abilities')} >
+              <h2 className='inner-projects-button'>
+                My Abilities
+              </h2>
+            </div>
+            <div className='right-projects-button bottom-right' onClick={() => this.props.onButtonClick('unavailable')} >
+              <h2 className='right-inner-projects-button'>
+                Coming Soon
+              </h2>
+            </div>
+          </div>
+        </Transition>
+      </div>
+    )
+
+  }
+}
+
+export default HomeButtons;
