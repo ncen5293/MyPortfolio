56aea5bb05ff6c3d59de1b8ede1a385de5da5a61
ncen5293 <nickycen@hotmail.com>
2019-08-23 16:57:52 -0400

    added some hobbies

--- client/src/components/HobbiesPage.js
+++ client/src/components/HobbiesPage.js
@@ -27,6 +27,25 @@ class HobbiesPage extends Component {
                         I like this
                       </Card.Description>
                     </Card.Content>
+                    <Card.Content extra>
+                      <p>
+                        (Further) Fire Emblem: Three Houses
+                      </p>
+                    </Card.Content>
+                  </Card>
+                  <Card raised color='blue'>
+                    <Card.Content>
+                      <Card.Header>Devil May Cry 5</Card.Header>
+                      <Card.Meta>Favorite Stylish Action Game</Card.Meta>
+                      <Card.Description>
+                        I like this
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <p>
+                        (Further) Dark Souls 3, Monster Hunter: World
+                      </p>
+                    </Card.Content>
                   </Card>
                 </Card.Group>
               </Grid.Column>
@@ -34,6 +53,36 @@ class HobbiesPage extends Component {
                 <Header as='h3' icon textAlign='center' dividing>
                   <Header.Content>Shows</Header.Content>
                 </Header>
+                <Card.Group itemsPerRow={2}>
+                  <Card raised color='red'>
+                    <Card.Content>
+                      <Card.Header>The Travelers</Card.Header>
+                      <Card.Meta>Favorite Sci-fi Show</Card.Meta>
+                      <Card.Description>
+                        I like this
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <p>
+                        (Further) Agents of S.H.I.E.L.D
+                      </p>
+                    </Card.Content>
+                  </Card>
+                  <Card raised color='red'>
+                    <Card.Content>
+                      <Card.Header>JoJo's Bizarre Adventure</Card.Header>
+                      <Card.Meta>Favorite Anime</Card.Meta>
+                      <Card.Description>
+                        I like this
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <p>
+                        (Further) Persona 4: The Animation
+                      </p>
+                    </Card.Content>
+                  </Card>
+                </Card.Group>
               </Grid.Column>
               <Grid.Column>
                 <Header as='h3' icon textAlign='center' dividing>
