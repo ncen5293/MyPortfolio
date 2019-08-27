a79ecf1c573254f0d8d67a8a834008bcb8379987
ncen5293 <nickycen@hotmail.com>
2019-08-27 15:30:27 -0400

    added unique like dialogue for certain hobbies

--- client/src/components/LikeModal.js
+++ client/src/components/LikeModal.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { Modal } from 'semantic-ui-react';
 import '../styles/Home.css';
 
+const dialogue = {
+  'Persona 5': 'Looking Cool, Joker!',
+  'Devil May Cry 5': 'SSS',
+  'Kirby Series': 'Poyo',
+  "JoJo's Bizarre Adventure": 'This is... Requiem'
+}
+
 class LikeModal extends Component {
   render() {
+    let likeDialogue = '';
+    if (dialogue[this.props.title]) {
+      likeDialogue = dialogue[this.props.title];
+    } else {
+      likeDialogue = 'Looking Cool, Joker!';
+    }
     return (
       <Modal basic
         open={this.props.open}
         onClose={this.props.onMenuToggle}
       >
         <Modal.Content>
-          Looking Cool, Joker!
+          {likeDialogue}
         </Modal.Content>
       </Modal>
     )
